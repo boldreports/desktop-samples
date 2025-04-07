d7468f4376bd2983b2d30abc53c9ea1787084aaa
Jeevalakshmi <jeevalakshmi.baskaran@syncfusion.com>
2025-04-07 20:16:23 +0530

    documentation(REPORT-418093): Moved the sample

---
+++ WPF/Report Viewer/WPF with WebView/BoldReportsAPI/Program.cs
@@ -0,0 +1,28 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// ✅ Register MemoryCache
+builder.Services.AddMemoryCache();
+
+// ✅ Register Controllers
+builder.Services.AddControllers();
+
+// ✅ Register Swagger (optional, for API documentation)
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+
+app.UseCors("AllowAll"); // Enable CORS
+app.UseAuthorization();
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
+
